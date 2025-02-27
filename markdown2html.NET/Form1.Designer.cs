951322d3780bf0ed103749a403307fe59f3f0515
Tobias Kisling <hasederhi.12@gmail.com>
2025-02-27 18:36:54 +0100

    Add project files.

---
+++ markdown2html.NET/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace markdown2html.NET
+{
+    partial class window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(window));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.webBrowserPreview = new System.Windows.Forms.WebBrowser();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label2.Location = new System.Drawing.Point(14, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Select markdown file:";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(157, 116);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(300, 20);
+            this.txtFilePath.TabIndex = 1;
+            this.txtFilePath.TextChanged += new System.EventHandler(this.txtFilePath_TextChanged);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(452, 116);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(101, 20);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnConvert.Location = new System.Drawing.Point(18, 465);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(150, 45);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "Convert to HTML";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // webBrowserPreview
+            // 
+            this.webBrowserPreview.Location = new System.Drawing.Point(18, 182);
+            this.webBrowserPreview.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserPreview.Name = "webBrowserPreview";
+            this.webBrowserPreview.Size = new System.Drawing.Size(535, 263);
+            this.webBrowserPreview.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label3.Location = new System.Drawing.Point(14, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Output Preview:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(121, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(294, 86);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "md2html";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(395, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "by tk_dev";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(18, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 72);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(317, 465);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(236, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "md2html.NET Version 1.0.0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(306, 490);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(247, 25);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "https://github.com/hasderhi";
+            // 
+            // window
+            // 
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(571, 529);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.webBrowserPreview);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "window";
+            this.Text = "markdown2html by tk_dev";
+            this.Load += new System.EventHandler(this.window_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.WebBrowser webBrowserPreview;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ markdown2html.NET/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace markdown2html.NET
+{
+    public partial class window: Form
+    {
+        public window()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Select Markdown File",
+                Filter = "Markdown Files (*.md)|*.md|All Files (*.*)|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFilePath.Text) || !File.Exists(txtFilePath.Text))
+            {
+                MessageBox.Show("Please select a valid Markdown file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                string markdownText = File.ReadAllText(txtFilePath.Text);
+                string htmlContent = ConvertMarkdownToHtml(markdownText);
+
+                // Save the HTML file
+                string htmlFilePath = Path.ChangeExtension(txtFilePath.Text, ".html");
+                File.WriteAllText(htmlFilePath, htmlContent);
+
+                // Load the HTML content into WebBrowser
+                webBrowserPreview.DocumentText = htmlContent;
+
+                MessageBox.Show($"Conversion successful!\nSaved to: {htmlFilePath}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Conversion Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        private void rtbPreview_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private string ConvertMarkdownToHtml(string markdown)
+        {
+            StringBuilder html = new StringBuilder();
+
+            // Define CSS styles
+            string cssStyles = @"
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #b3b3b3;
+                text-align: left;
+                padding-left: 10%;
+            }
+            .img {
+                width: 80%;
+            }
+        </style>";
+
+            // Start building the HTML
+            html.AppendLine("<html>");
+            html.AppendLine("<head>");
+            html.AppendLine("<meta charset=\"UTF-8\">");  // Ensure UTF-8 support
+            html.AppendLine(cssStyles);  // Add CSS styles
+            html.AppendLine("</head>");
+            html.AppendLine("<body>");
+
+            string[] lines = markdown.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            foreach (string line in lines)
+            {
+                string trimmedLine = line.Trim();
+
+                // Convert headers
+                if (trimmedLine.StartsWith("# "))
+                    html.AppendLine($"<h1>{trimmedLine.Substring(2)}</h1>");
+                else if (trimmedLine.StartsWith("## "))
+                    html.AppendLine($"<h2>{trimmedLine.Substring(3)}</h2>");
+                else if (trimmedLine.StartsWith("### "))
+                    html.AppendLine($"<h3>{trimmedLine.Substring(4)}</h3>");
+                else if (trimmedLine.StartsWith("#### "))
+                    html.AppendLine($"<h4>{trimmedLine.Substring(5)}</h4>");
+                else if (trimmedLine.StartsWith("##### "))
+                    html.AppendLine($"<h5>{trimmedLine.Substring(6)}</h5>");
+                else if (trimmedLine.StartsWith("###### "))
+                    html.AppendLine($"<h6>{trimmedLine.Substring(7)}</h6>");
+
+                // Convert unordered lists
+                else if (trimmedLine.StartsWith("- ") || trimmedLine.StartsWith("* "))
+                    html.AppendLine($"<li>{trimmedLine.Substring(2)}</li>");
+
+                // Convert ordered lists
+                else if (trimmedLine.Length > 2 && char.IsDigit(trimmedLine[0]) && trimmedLine[1] == '.')
+                    html.AppendLine($"<li>{trimmedLine.Substring(2)}</li>");
+
+                // Convert blockquotes
+                else if (trimmedLine.StartsWith("> "))
+                    html.AppendLine($"<blockquote>{trimmedLine.Substring(2)}</blockquote>");
+
+                // Convert images ![alt](url)
+                else if (Regex.IsMatch(trimmedLine, @"!\[(.*?)\]\((.*?)\)"))
+                {
+                    trimmedLine = Regex.Replace(trimmedLine, @"!\[(.*?)\]\((.*?)\)", "<img class='img' src='$2' alt='$1'>");
+                    html.AppendLine(trimmedLine);
+                }
+
+                // Convert normal paragraphs (apply bold & italic inside)
+                else if (!string.IsNullOrWhiteSpace(trimmedLine))
+                {
+                    // Convert bold (**bold text**)
+                    trimmedLine = Regex.Replace(trimmedLine, @"\*\*(.*?)\*\*", "<b>$1</b>");
+
+                    // Convert italic (*italic text*)
+                    trimmedLine = Regex.Replace(trimmedLine, @"\*(.*?)\*", "<i>$1</i>");
+
+                    html.AppendLine($"<p>{trimmedLine}</p>");
+                }
+            }
+
+            // Close the body and HTML
+            html.AppendLine("</body>");
+            html.AppendLine("</html>");
+
+            return html.ToString();
+        }
+
+        private void txtFilePath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void window_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
